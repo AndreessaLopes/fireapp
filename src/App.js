510bff2e54eb82a89dec4c6883cec6016bbdcf1a
Andressa <andressacaroline082011@gmail.com>
2024-12-30 18:37:56 -0300

    Criando novo app com firebase

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { db } from "./firebaseConnection";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello World </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebaseConnection.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBN3llxanS4bmLgpzl6Kyy3OF_VizfJ7Jk",
+  authDomain: "curso-20cd8.firebaseapp.com",
+  projectId: "curso-20cd8",
+  storageBucket: "curso-20cd8.firebasestorage.app",
+  messagingSenderId: "849914028913",
+  appId: "1:849914028913:web:a2a568bc947ecac05c0cd0",
+  measurementId: "G-3RN8H1YD5F",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+export { db };
